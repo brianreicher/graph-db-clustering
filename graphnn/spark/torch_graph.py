0f0c91c7732e4b12639bfce8583e7225a18c1bd7
brianreicher <reicher.b@northeastern.edu>
2023-04-09 19:08:46 -0400

    GNN Model and Trainer base builds

--- graphnn/spark/torch_graph.py
+++ graphnn/spark/torch_graph.py
@@ -13,17 +13,31 @@ class TorchImageGraph():
     
     def extract_graph(self) -> None:
         query:str = f"""
-                        MATCH (n:Image)-[]-(m)
-                        WHERE n.name = "{self.name}"
-                        RETURN id(n) as source, id(m) as target
+                        MATCH (n)-[e]-(m)
+                        WHERE n.graph_name = "{self.name}"
+                        RETURN id(n) as source, id(m) as target, n.f1 as f1, n.f2 as f2, n.f3 as f3, n.f4 as f4, n.f5 as f5, e.weight as weight
                     """
 
+        # Execute the query and extract the nodes and edges
         with self.database.driver.session() as session:
             result = session.run(query)
-            edges = [(record["source"], record["target"]) for record in result.records()]
+            nodes:set = set()
+            edges:list = []
+            for record in result.records():
+                source = record["source"]
+                target = record["target"]
+                f1 = record["f1"]
+                f2 = record["f2"]
+                f3 = record["f3"]
+                f4 = record["f4"]
+                f5 = record["f5"]
+                weight = record["weight"]
+                nodes.add(source)
+                nodes.add(target)
+                edges.append((source, target, weight, f1, f2, f3, f4, f5))
 
-            # Convert the edges to PyTorch Geometric format
-            edge_index: torch.Tensor = torch.tensor(edges).t().contiguous()
-
-            # Assign a PyTorch Geometric Data object
-            self.graph: Data =Data(edge_index=edge_index)
+        # Convert the nodes and edges to a PyTorch Geometric graph
+        edge_index: torch.Tensor = torch.tensor(edges)[:, :2].t().contiguous()
+        edge_attr: torch.Tensor = torch.tensor(edges)[:, 2:].float()
+        x: torch.Torch = torch.tensor(list(nodes)).unsqueeze(1).float()
+        data:Data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
