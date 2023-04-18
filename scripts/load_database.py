beec2b453eedbe8c9e76a00b9081ccb172207e8a
brianreicher <reicher.b@northeastern.edu>
2023-04-18 15:02:34 -0400

    Remove old scripts

--- scripts/load_database.py
+++
@@ -1,8 +0,0 @@
-from ..graphnn.extract_features import FeatureExtractor
-
-if __name__ == "__main__":
-    print("initialize driver")
-
-    fe: FeatureExtractor = FeatureExtractor(image_dir='data', batch_size=1)
-    fe.spark.stop()
-    print('initialized driver')
